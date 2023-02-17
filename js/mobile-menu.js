3782a19be38b2f09ec50c2031b3d1436ad5bb43c
DanyaGTx <raxub007@gmail.com>
2023-02-17 11:37:28 +0200

    fixed small bugs

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 const closeMenuBtns = document.querySelectorAll(".header-navigation-item-dropdown-content-list__close");
 const popUpMenus = document.querySelectorAll(".header-navigation-item-dropdown");
 const links = document.querySelectorAll(".header-navigation-item__link");
+const closeMobileMenuBurger = document.querySelector('.burger__toggler')
 if (window.matchMedia("(max-width: 840px)").matches) {
     for (let i = 0; i < links.length; i++) {
         links[i].addEventListener("click", function (event) {
@@ -14,8 +15,10 @@ if (window.matchMedia("(max-width: 840px)").matches) {
     
             if (popUpMenus[i].style.display === "block") {
                 popUpMenus[i].style.display = "none";
+                document.body.style.overflow = 'auto';
             } else {
                 popUpMenus[i].style.display = "block";
+                document.body.style.overflow = 'hidden';
             }
     
             closeMenuBtns[i].addEventListener("click", () => {
@@ -27,10 +30,14 @@ if (window.matchMedia("(max-width: 840px)").matches) {
     
     const closeMenu = (id) => {
         popUpMenus[id].style.display = "none";
+        document.body.style.overflow = 'auto';
     };
-    
 
-
-    
+    closeMobileMenuBurger.addEventListener('click', () => {
+        if(!closeMobileMenuBurger.checked) {
+            document.body.style.overflow = 'auto';
+        }
+    })
+   
 }
 
