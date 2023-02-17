156c66291d08b2e600648471c67583549fb8fb87
DanyaGTx <raxub007@gmail.com>
2023-02-17 15:46:49 +0200

    fixed bug with video popup

--- js/video-popup.js
+++ js/video-popup.js
@@ -1,7 +1,7 @@
 const videoBtn = document.querySelector('.video-btn');
 const videoPopup = document.querySelector('.video-popup');
 const videoPlayer = document.querySelector('.video-player');
-
+const playerOptions = '?autoplay=1&enablejsapi=1&version=3&playerapiid=ytplayer'
 
 window.addEventListener('click', (event) => {
     if (event.target == videoPopup) {
@@ -12,12 +12,14 @@ window.addEventListener('click', (event) => {
 videoBtn.addEventListener('click', () => {
     videoPopup.style.display = 'block';
     document.body.style.overflow = 'hidden';
-    videoPlayer.src = videoBtn.getAttribute('data-video-url')
+    videoPlayer.src = videoBtn.getAttribute('data-video-url') + playerOptions
+    
 });
 
 const closePopUp = () => {
     videoPopup.style.display = 'none';
     document.body.style.overflow = 'auto';
+    videoPlayer.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 
 
